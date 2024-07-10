57e1b3a22f68b2f15b7f5701dde426828eb5a41b
vaneck237 <vaneckbilongo@gmail.com>
2024-07-10 15:18:40 +0100

    fix timeout to 1000s

--- app/src/main/java/org/openimis/imispolicies/network/util/OkHttpUtils.java
+++ app/src/main/java/org/openimis/imispolicies/network/util/OkHttpUtils.java
@@ -32,9 +32,9 @@ public class OkHttpUtils {
             synchronized (OkHttpUtils.class) {
                 if (client == null) {
                     OkHttpClient.Builder builder = new OkHttpClient.Builder();
-                    builder.connectTimeout(30, TimeUnit.SECONDS)
-                            .writeTimeout(30,TimeUnit.SECONDS)
-                            .readTimeout(30, TimeUnit.SECONDS);
+                    builder.connectTimeout(1000, TimeUnit.SECONDS)
+                            .writeTimeout(1000,TimeUnit.SECONDS)
+                            .readTimeout(1000, TimeUnit.SECONDS);
                     HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
                     interceptor.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.BASIC);
                     builder.addInterceptor(interceptor);
