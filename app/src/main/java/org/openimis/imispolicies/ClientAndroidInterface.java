c005870025192ab084e6da6ad0a3072ff4323623
vaneck237 <vaneckbilongo@gmail.com>
2025-01-29 17:47:17 +0100

    print version in about

--- app/src/main/java/org/openimis/imispolicies/ClientAndroidInterface.java
+++ app/src/main/java/org/openimis/imispolicies/ClientAndroidInterface.java
@@ -5511,4 +5511,9 @@ public class ClientAndroidInterface {
                 (d, i) -> new Thread(() -> Log.zipLogFiles(activity)).start()
         );
     }
+
+    @JavascriptInterface
+    public String getVersion(){
+        return BuildConfig.VERSION_NAME;
+    }
 }
