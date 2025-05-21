df626af654b5fbc9d20a09d2620729f66e57216b
vaneck237 <vaneckbilongo@gmail.com>
2025-05-21 13:45:10 +0100

    upgrade timeout

--- app/src/main/java/org/openimis/imispolicies/network/util/OkHttpUtils.java
+++ app/src/main/java/org/openimis/imispolicies/network/util/OkHttpUtils.java
@@ -32,9 +32,9 @@ public class OkHttpUtils {
             synchronized (OkHttpUtils.class) {
                 if (client == null) {
                     OkHttpClient.Builder builder = new OkHttpClient.Builder();
-                    builder.connectTimeout(1000, TimeUnit.SECONDS)
-                            .writeTimeout(1000,TimeUnit.SECONDS)
-                            .readTimeout(1000, TimeUnit.SECONDS);
+                    builder.connectTimeout(10000, TimeUnit.SECONDS)
+                            .writeTimeout(100000,TimeUnit.SECONDS)
+                            .readTimeout(100000, TimeUnit.SECONDS);
                     HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
                     interceptor.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.BASIC);
                     builder.addInterceptor(interceptor);
